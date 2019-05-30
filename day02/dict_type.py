f24106281939fba72933674203fcb797a2b26d1a
韩斌 <517770250@qq.com>
2019-05-30 17:24:19 +0800

    第二天

--- day02/dict_type.py
+++ day02/dict_type.py
@@ -3,6 +3,29 @@ adict = {'username':'admin','password':'123456'}
 def dict_sel():
     print(adict['password'])
 
+def dict_updat():
+    adict['username']='hb'
+    print(adict)
+
+def dict_del():
+    adict.pop('password')
+    print(adict)
+
+def dict_add():
+    # 增加
+    adict['age']= 21
+    print(adict)
+    # 合并一
+    bdict = {'list':[12,4],'tuple':[5,4]}
+    adict.update(bdict)
+    print(adict)
+    # 合并二
+    cdict ={'password':'77777','class':'1904'}
+    ddict =dict(adict,**cdict)
+    print(ddict)
 
 if __name__ == '__main__':
-    dict_sel()
+    # dict_sel()
+    # dict_updat()
+    # dict_del()
+    dict_add()
